d730fa83fc79d7f51e0ffd628a596beb43132791
gunny6026 <gunny6026@naver.com>
2021-02-11 14:59:07 +0900

    컴포넌트 디자인 패턴 - controlled

---
+++ 인프런 강의/3.고급(실습,리팩토링)/vue_design/controlled/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- props 로 데이터를 넘기면 생기는 문제점을  컴포넌트에 v-model로 내려보았다.  -->
+  <!-- #1  -->
+  <!-- <check-box :propsCheck="checked"></check-box> -->
+
+  <!-- #2  props 대신 v-model로 해결  -->
+  <check-box v-model="checked"></check-box>
+</template>
+
+<script>
+import CheckBox from './components/CheckBox.vue'
+export default {
+  components: { CheckBox },
+
+  data(){
+    return {
+    //  checked: "안녕",
+     checked : false
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ 인프런 강의/3.고급(실습,리팩토링)/vue_design/controlled/src/components/CheckBox.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+        <!-- #1  -->
+  <!-- <input type="checkbox"  v-model="propsCheck"> -->
+
+  <!-- <input type="text" v-model="propsCheck"> -->
+  <!-- {{propsCheck}} -->
+
+
+      <!-- #2 로 해결 -->
+    <input type="checkbox" :value="value" @click="toggleCheck" >
+  </div>
+
+  
+</template>
+
+<script>
+export default {
+  // props: ['propsCheck'],
+  
+  // data(){
+  //   return{
+  //     propsCheck : "하위"
+  //   }
+  // }
+
+  // created(){
+  //   console.log(this.propsCheck);
+  // }
+
+    props : ['value'],
+    methods : {
+      toggleCheck(){
+        this.$emit('input' , !this.value);
+      }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
